a31381b307138d0961107033b03dbcb86ae220d5
alexandergaskins <alexandergaskins@ufl.edu>
2026-02-06 22:53:50 -0500

    Add files via upload

--- R/plot_height.R
+++ R/plot_height.R
@@ -1,47 +1,80 @@
-library(lidR)
-library(rgl)
-
-LASfile <- "C:/Users/alexandergaskins/Documents/FuelDeep3D/inst/extdata/las/trees.laz"
-rLAS <- readLAS(LASfile)
-
-myColorRamp <- function(colors, values) {
-  v <- (values - min(values)) / diff(range(values))
-  x <- colorRamp(colors)(v)
-  rgb(x[,1], x[,2], x[,3], maxColorValue = 255)
-}
-
-zvals <- rLAS$Z
-col <- myColorRamp(c("blue", "green", "yellow", "red"), zvals)
-
-X <- rLAS$X - min(rLAS$X)
-Y <- rLAS$Y - min(rLAS$Y)
-Z <- rLAS$Z - min(rLAS$Z)
-
-rgl.clear()
-bg3d("white")
-
-points3d(X, Y, Z, col = col, size = 0.5)
-
-xpos <- max(X) + 0.05 * max(X)
-ypos <- min(Y)
-
-zseq <- seq(min(Z), max(Z), length.out = 200)
-cols_bar <- myColorRamp(c("blue", "green", "yellow", "red"), zseq)
-
-for (i in 1:(length(zseq)-1)) {
-  quads3d(
-    c(xpos, xpos + 0.02 * max(X), xpos + 0.02 * max(X), xpos),
-    c(ypos, ypos, ypos, ypos),
-    c(zseq[i], zseq[i], zseq[i+1], zseq[i+1]),
-    col = cols_bar[i],
-    lit = FALSE
+#' Plot a 3D LAS point cloud with elevation color ramp and legend
+#'
+#' @param las A LAS object (from lidR).
+#' @param colors A vector of colors for the elevation ramp.
+#' @param point_size Numeric. Size of plotted points.
+#' @param zoom Numeric. rgl zoom level.
+#'
+#' @return Invisibly returns NULL.
+#' @export
+plot_las_elevation_3d <- function(
+    las,
+    colors = c("blue", "green", "yellow", "red"),
+    point_size = 0.5,
+    zoom = 0.6
+) {
+  
+  if (!inherits(las, "LAS")) {
+    stop("Input must be a LAS object.")
+  }
+  
+  if (!requireNamespace("rgl", quietly = TRUE)) {
+    stop("Package 'rgl' is required.")
+  }
+  
+  # Normalize coordinates
+  X <- las$X - min(las$X)
+  Y <- las$Y - min(las$Y)
+  Z <- las$Z - min(las$Z)
+  
+  # Color ramp helper
+  myColorRamp <- function(colors, values) {
+    v <- (values - min(values)) / diff(range(values))
+    x <- grDevices::colorRamp(colors)(v)
+    grDevices::rgb(x[,1], x[,2], x[,3], maxColorValue = 255)
+  }
+  
+  col <- myColorRamp(colors, Z)
+  
+  rgl::rgl.clear()
+  rgl::bg3d("white")
+  
+  rgl::points3d(X, Y, Z, col = col, size = point_size)
+  
+  # Color bar
+  xpos <- max(X) + 0.05 * max(X)
+  ypos <- min(Y)
+  
+  zseq <- seq(min(Z), max(Z), length.out = 200)
+  cols_bar <- myColorRamp(colors, zseq)
+  
+  for (i in seq_len(length(zseq) - 1)) {
+    rgl::quads3d(
+      c(xpos, xpos + 0.02 * max(X), xpos + 0.02 * max(X), xpos),
+      c(ypos, ypos, ypos, ypos),
+      c(zseq[i], zseq[i], zseq[i+1], zseq[i+1]),
+      col = cols_bar[i],
+      lit = FALSE
+    )
+  }
+  
+  rgl::text3d(
+    xpos + 0.04 * max(X),
+    ypos,
+    min(Z),
+    texts = round(min(Z), 2),
+    adj = c(0,0)
   )
+  
+  rgl::text3d(
+    xpos + 0.04 * max(X),
+    ypos,
+    max(Z),
+    texts = round(max(Z), 2),
+    adj = c(0,1)
+  )
+  
+  rgl::view3d(theta = 0, phi = -90, zoom = zoom)
+  
+  invisible(NULL)
 }
-
-text3d(xpos + 0.04 * max(X), ypos, min(Z),
-       texts = round(min(Z),2), adj = c(0,0))
-
-text3d(xpos + 0.04 * max(X), ypos, max(Z),
-       texts = round(max(Z),2), adj = c(0,1))
-
-view3d(theta = 0, phi = -90, zoom = 0.55)
